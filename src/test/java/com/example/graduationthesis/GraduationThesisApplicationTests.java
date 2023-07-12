807566fdd58ee49572675d6094eebfc53edf2fad
Jangwoo No <ssk01017@gmail.com>
2023-07-12 14:08:25 +0900

    aa

---
+++ src/test/java/com/example/graduationthesis/GraduationThesisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graduationthesis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationThesisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

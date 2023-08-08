9105a1b1d6ce9f93fdb03b2079275c99335b1340
given-dragon <yijy001@gmail.com>
2023-08-08 14:11:47 +0900

    유저 인증기능 추가

---
+++ src/main/java/com/example/graduationthesis/data/dto/ImgDto.java
@@ -0,0 +1,14 @@
+package com.example.graduationthesis.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@AllArgsConstructor
+public class ImgDto {
+    private String uuid;
+    private String fileName;
+
+}

316112d25b636b738d3252803e13c51cc1ef0314
given-dragon <yijy001@gmail.com>
2023-08-12 18:33:52 +0900

    attendance controller 불필요한 코드 삭제

--- src/main/java/com/example/graduationthesis/controller/AttendanceController.java
+++ src/main/java/com/example/graduationthesis/controller/AttendanceController.java
@@ -2,21 +2,14 @@ package com.example.graduationthesis.controller;
 
 import com.example.graduationthesis.data.dto.ImgDto;
 import com.example.graduationthesis.service.AttendanceService;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
 import java.io.IOException;
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -33,7 +26,6 @@ public class AttendanceController {
             @RequestPart(value = "studentNum") String studentNum,
             @RequestPart(value = "lectureCode") String lectureCode,
             @RequestPart(value = "faceImage") MultipartFile faceImage) throws IOException, ParseException {
-        // TODO: 2023/07/21 얼굴 인증 서버에 사진과 학번을 넘기고 호출하기
 
         ImgDto imgDto = new ImgDto(
                 UUID.randomUUID().toString(),
@@ -42,7 +34,6 @@ public class AttendanceController {
         File saveFile = new File(imgDto.getUuid() + "_" + imgDto.getFileName());
         faceImage.transferTo(saveFile);
 
-//        JSONObject responseJson = attendanceService.sendDataForValidation("20184114", location+"/"+saveFile.getName());
         JSONObject responseJson = attendanceService.sendDataForValidation("20184114", location+"/"+saveFile.getName());
 
         if (responseJson.get("validation_result").equals("True")) {
